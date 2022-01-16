4e44024c3e161aa076dd3f91cc450bae42c79a27
iamzsombi <zsombinagy.14@gmail.com>
2022-01-16 21:01:50 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from "./containers";
+import {CTA, Brand, Navbar} from "./components";
+import './App.css'
+
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className="App">
+            <div className="gradient_bg">
+                <Navbar />
+                <Header />
+
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
+
     )
 }
 
